f4f5600124445935b6203769d4b26ab5c646acc3
RodrigoLee56 <sousarodrigo56@outlook.com>
2022-07-26 16:53:00 -0300

    Subindo os primeiros imports

---
+++ install.py
@@ -0,0 +1,16 @@
+import os
+
+os.environ["MPLCONFIGDIR"] = os.getcwd() + "/configs/"
+
+import tensorflow as tf
+from tensorflow.python.compiler.tensorrt import trt_convert as trt
+from tensorflow import keras
+import json
+import numpy as np
+from pathlib import Path
+
+# Install pose estimation model https://github.com/NVIDIA-AI-IOT/trt_pose
+import trt_pose.coco
+import trt_pose.models
+import torch
+import torch2trt
